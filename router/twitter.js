3f7f8caa79e9fe346e562a18516b10217d254e85
infi000-home <113079767@qq.com>
2016-12-19 23:17:56 +0800

    update twitter

--- router/twitter.js
+++ router/twitter.js
@@ -1,26 +1,29 @@
-var express=require("express");
-var cheerio=require("cheerio");
-var superagent=require("superagent");
-var fs=require("fs");
+var express = require("express");
+var cheerio = require("cheerio");
+var superagent = require("superagent");
+var fs = require("fs");
 
-var website="https://twitter.com/infi00002/lists/player";
-var router=express.Router();
+var website = "https://twitter.com/infi00002/lists/player";
+var router = express.Router();
 
-router.get("/:id",function(req,res,next){
-	if(req.params.id=="1"){
-		superagent.get(website)
-			   .end(function(err,sres){
-			   	if(err){
-			   		return next(err);
-			   	}
-			   	//加载并且转码
-			   	var $=cheerio.load(sres.text, { decodeEntities: false });
-			   	var items=[];
-                              $("#stream-items-id .js-stream-item[data-item-type='tweet']").each(function(){})
-			 res.send($("#stream-items-id .js-stream-item[data-item-type='tweet']").html())
-			   })	
-	}
-	
+router.get("/:id", function(req, res, next) {
+    if (req.params.id == "1") {
+        superagent.get(website)
+            .end(function(err, sres) {
+                if (err) {
+                    return next(err);
+                }
+                //加载并且转码
+                var $ = cheerio.load(sres.text, { decodeEntities: false });
+                var items = "";
+                $("#stream-items-id .js-stream-item[data-item-type='tweet']").each(function(index,element) {
+                	var $element=$(element);
+                	items+=$element.html();
+                })
+                res.send(items);
+            })
+    }
+
 });
 
 module.exports = router;
