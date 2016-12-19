4dac59ff8a41f0583f4e913d1f97cfeddc0653a3
infi000-home <113079767@qq.com>
2016-12-19 23:45:19 +0800

    updata twitter

--- router/twitter.js
+++ router/twitter.js
@@ -3,7 +3,7 @@ var cheerio = require("cheerio");
 var superagent = require("superagent");
 var fs = require("fs");
 
-var website = "https://twitter.com/infi00002/lists/player";
+var website = "https://twitter.com/infi00002/lists/player/members";
 var router = express.Router();
 
 router.get("/:id", function(req, res, next) {
@@ -15,10 +15,17 @@ router.get("/:id", function(req, res, next) {
                 }
                 //加载并且转码
                 var $ = cheerio.load(sres.text, { decodeEntities: false });
-                var items = "";
-                $("#stream-items-id .js-stream-item[data-item-type='tweet']").each(function(index,element) {
+                var items = [];
+                $("#stream-items-id .js-stream-item[data-item-type='user']").each(function(index,element) {
                 	var $element=$(element);
-                	items+=$element.html();
+                	var _player=$element.find(".fullname").html();
+                	var _info=$element.find(".bio").html();
+                	var _head=$element.find(".avatar").attr("src")
+                	items.push({
+                		player:_player,
+                		info:_info,
+                		head:_head
+                	});
                 })
                 res.send(items);
             })
